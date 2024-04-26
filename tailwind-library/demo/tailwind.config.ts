09fd3b92e340bddac12e544fc8ae3ca0bad458b2
ashanfernando <ashan256@gmail.com>
2024-04-26 13:41:05 +0530

    Textending the theme in app

--- tailwind-library/demo/tailwind.config.ts
+++ tailwind-library/demo/tailwind.config.ts
@@ -8,7 +8,23 @@ const bitPlugin = new Plugin({
 
 const config: Config = {
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        sans: ['Comic Sans MS', 'cursive', 'sans-serif'], // Default sans-serif font
+      },
+      colors: {
+        'blue-500': '#00d1ff', // Fluorescent blue
+        'blue-700': '#0094cc', // Darker fluorescent blue
+        'bg-gray-200': '#0094cc',
+        'gray-200': '#ccff00', // Fluorescent green for background
+        'blue-300': '#33ccff', // Fluorescent blue for focus ring
+        'blue-800': '#ff00ff', // Fluorescent pink for dark mode focus ring
+        'blue-600': '#00ffcc', // Fluorescent teal for checked background
+      },
+      ringColor: {
+        'blue-300': '#33ccff', // Matching focus ring with fluorescent blue
+      }
+    },
   },
   plugins: [bitPlugin.apply()],
 };
